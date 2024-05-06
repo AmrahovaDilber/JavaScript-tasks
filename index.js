9adfd254399f0b4b3da9b22ebf594610c550a894
AmrahovaDilber <amrahovad911@gmail.com>
2024-05-06 12:16:52 +0400

    task2

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function getNumber(number) {
 }
 getNumber(15);
 getNumber(30);
+
+//TASK2
+//Implement a function that checks if a given string contains both the letter 'a' and the letter 'b'.
+function getString(str) {
+  if (str.toLowerCase().includes("a" && "b")) {
+    console.log(`Yes,${str} includes both "a" and "b"`);
+  } else {
+    console.log(`No,${str} doesn't include both "a" and "b" `);
+  }
+}
+getString("Porsche");
+getString("Banana");
